7ae261110f030bf445196b3a9e468cd84f21bc9a
Avishkar2004 <avishkarkakde2004@gmail.com>
2025-04-09 13:29:30 +0530

    Updates

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import AppRouter from "../src/Router/Router.jsx";
+
+function App() {
+  return (
+    <div>
+      <AppRouter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Router/Router.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { BackgroundBeamsWithCollision } from '../components/ui/background-beams-with-collision'
+
+function AppRouter() {
+    return (
+        <BackgroundBeamsWithCollision />
+    )
+}
+
+export default AppRouter
